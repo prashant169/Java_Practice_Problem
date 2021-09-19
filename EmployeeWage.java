cbe49c41a7b6afd86e71979078c7b2e18e64de3c
prashant169 <prashantphad2018@gmail.com>
2021-09-19 09:01:08 -0700

    Calculate Wage For Month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,41 +1,46 @@
 import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class EmployeeWage{
         public static void main( String []args )
         {
                 System.out.println("Welcome");
-                Jadeja();
+                Dhoni();
         }
-        static void Jadeja(){
-                Scanner in = new Scanner(System.in);
-                int PartTimeHour = 4;
-                int RatePerHour = 20;
-                int FullDayHour = 8;
-                int Day = 0 ;
-                Random rn = new Random ();
-                int rand =rn.nextInt(3);
-                        switch(rand){
-                                        case 1:
-                                                System.out.println("IT's Full Time Wages: ");
-                                                System.out.println("Enter NUmber Of Working Day's: ");
-                                                Day = in.nextInt();
-                                                int FullTimeWages = RatePerHour * (FullDayHour * Day);
-                                                System.out.println("FullTimeWages for " + Day +" Days: " + "Rs." + FullTimeWages);
-                                                break;
-                       
-                                        case 2:
-                                                System.out.println("IT's Part Time Wages: ");
-                                                System.out.println("Enter Number Of Working Day's: ");
-                                                Day = in.nextInt();
-                                                int PartTimeWages = Day * PartTimeHour * FullDayHour;
-                                                System.out.println("PartTimeWages for " + Day +" days: " + "Rs." + PartTimeWages);
-                                                break;
-                                        default:
-                                                System.out.println("Employee is Absent..........!");
-                                                break;
-                                     }
-                        in.close();
-        		}
-        }
+
+                static void Dhoni(){
+                                        int TotalWorkingHour = 8 ;
+                                        int TotalWorkingDay  = 20 ;
+                                        int RatePerHour = 20 ;
+                                        int IsPartTime = 2 ;
+                                        int IsFullTime = 1 ;
+                                        int RatePerhour = 4 ;
+
+
+                                        Random rn = new Random();
+                                        int rand = rn.nextInt(3);
+                                        if (rand == 0)
+                                        {
+                                                System.out.println("----------This is Full time Work in Month---------");
+                                                int TotaEmployeeWage = TotalWorkingHour * TotalWorkingDay * RatePerHour ;
+                                                System.out.println("Total Employee Wages is: " + TotaEmployeeWage);
+                                        }
+                                        else if (rand == 0)
+                                        {
+                                                System.out.println("-------This is Part Time Work in Month--------");
+                                                int TotaEmployeeWage = TotalWorkingHour * TotalWorkingDay * RatePerhour;
+                                                System.out.println("Total Employee Wages is: "+ TotaEmployeeWage);
+                                        }
+                                        else
+                                        {
+                                                System.out.println("----Employee is Absent------");
+                                        }
+
+
+
+                           }
+
+
+}
+
