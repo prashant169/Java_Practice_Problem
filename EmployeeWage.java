012a2ba9dd5d1c32b5b70548be5e816d3c68e2e8
prashant169 <prashantphad2018@gmail.com>
2021-09-19 08:41:10 -0700

    calculateDailyWage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,26 +1,30 @@
 import java.util.Random;
 
-	public class EmployeeWage {
-		public static void main( String args[] ){
+public class EmployeeWage {
+	public static void main( String args[] ){
 		System.out.println("Welcome To The Employee Wages");
-		Attendance();
-	}
+		CalculateDailyWages();
+}
 
-public static void Attendance()
+public static void CalculateDailyWages()
 {
 	int IsFullTime = 1;
 	Random rn = new Random();
 	int check= rn.nextInt(2);
-	
+	int RatePerHour = 20;
+	int FullDayHour = 8;
+	int TotalWages = 0;
 	if(check == IsFullTime)
 	{
 		System.out.println("Employee Present.");
+		TotalWages = RatePerHour * FullDayHour;
+		System.out.println("Daily Wages  : "+ TotalWages);
+		
 	}
 	else
  	{
 		System.out.println("Employee Absent.");
 	}
-	}
+}
 
 } 
-
