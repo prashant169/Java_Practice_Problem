d4601245dd3f1d80a70dfcd9b63021c4b6f92bee
prashant169 <prashantphad2018@gmail.com>
2021-09-19 09:08:13 -0700

    Calculate Wage Using Till condition

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,46 +1,64 @@
 import java.util.Random;
-import java.util.*;
+import java.util.Scanner;
 
+			public class EmployeeWage {
 
-public class EmployeeWage{
-        public static void main( String []args )
-        {
-                System.out.println("Welcome");
-                Dhoni();
-        }
+			public static final int FullDayHour = 8;
+			public static final int WorkingDayMonth = 20;
+			public static final int maxHoursInMonth = 100;
+			public static final int perHour = 20;
 
-                static void Dhoni(){
-                                        int TotalWorkingHour = 8 ;
-                                        int TotalWorkingDay  = 20 ;
-                                        int RatePerHour = 20 ;
-                                        int IsPartTime = 2 ;
-                                        int IsFullTime = 1 ;
-                                        int RatePerhour = 4 ;
+	public static void main(String x[]) {
 
+		//UC6_Employee_Wages _Till The Condition
+		MonthlyWages();
+	}
 
-                                        Random rn = new Random();
-                                        int rand = rn.nextInt(3);
-                                        if (rand == 0)
-                                        {
-                                                System.out.println("----------This is Full time Work in Month---------");
-                                                int TotaEmployeeWage = TotalWorkingHour * TotalWorkingDay * RatePerHour ;
-                                                System.out.println("Total Employee Wages is: " + TotaEmployeeWage);
-                                        }
-                                        else if (rand == 0)
-                                        {
-                                                System.out.println("-------This is Part Time Work in Month--------");
-                                                int TotaEmployeeWage = TotalWorkingHour * TotalWorkingDay * RatePerhour;
-                                                System.out.println("Total Employee Wages is: "+ TotaEmployeeWage);
-                                        }
-                                        else
-                                        {
-                                                System.out.println("----Employee is Absent------");
-                                        }
+	static void MonthlyWages() {
+		int fullTimeDays = 0;
+		int partTimeDays = 0;
+		int totalempHour = 0;
+		int totalWorkingDay = 0;
+		int absent = 0;
 
+		while (maxHoursInMonth > totalempHour && WorkingDayMonth > totalWorkingDay) {
 
+			totalWorkingDay++;
 
-                           }
+			Random rn = new Random();
+			int rand = rn.nextInt(3);
 
+			switch (rand) {
 
+			case 0:
+				fullTimeDays++;
+				totalempHour = totalempHour + FullDayHour;
+				break;
+			case 1:
+				partTimeDays++;
+				totalempHour = totalempHour + (FullDayHour / 2);
+				break;
+			default:
+				absent++;
+				break;
+				
+			}
+		}
+		int FullTimeSalary = perHour * (FullDayHour * fullTimeDays);
+		int PartTimeSalary = (partTimeDays * (FullDayHour / 2)) * perHour;
+
+		System.out.println("------------Employee_Wages-------------");
+		System.out.println("---------------------------------------");
+		System.out.println("FullTime_Days_in_month: " + fullTimeDays);
+		System.out.println("PartTime_Days_in_month: " + partTimeDays);
+		System.out.println("Absent_Days_in_month: " + absent);
+		System.out.println("Total_working_Days_IN_month : " + (fullTimeDays + partTimeDays));
+		System.out.println("Total_Hours    :===> " + totalempHour);
+		System.out.println("FullTime_Salary:===> " + FullTimeSalary);
+		System.out.println("PartTime_Salary:===> " + PartTimeSalary);
+		System.out.println("Total_Salary   :===> " + (FullTimeSalary + PartTimeSalary));
+		System.out.println("---------------------------------------");
+		System.out.println(totalWorkingDay);
+
+	}
 }
-
